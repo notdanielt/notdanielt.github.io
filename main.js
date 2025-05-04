295046cb1e52f47a64d72fe5d795af13f52a9ca8
-Danielt- <164779581+notdanielt@users.noreply.github.com>
2025-05-03 23:01:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-var timer = 0;
 function clickjQuery(el) {el[0].click()}
-function goto(link) {clickjQuery($("<a href='"+link+"' target='_blank'></a>"))}
+function goto(link) {clickjQuery($("<a href='"+link+"'></a>"))}
 function switchpage(page) {
-  document.querySelectorAll(".page").forEach(function(p) {
-    p.style.display = "none";
-  })
+  document.querySelectorAll(".page").forEach(function(p) {p.style.display = "none";})
   document.getElementById(page).style.display = "";
   window.scrollTo(0, 0);
 }
